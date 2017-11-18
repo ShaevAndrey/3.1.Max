80f3f50f2b9a3b2447564baddac7fad690c2e13c
ShaevAndrey <spinoza33@yandex.ru>
2017-11-18 21:19:42 +0300

    3.1.Max

--- MaxTest.java
+++ MaxTest.java
@@ -4,7 +4,12 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+/**
+ * Class MaxTest 3.1. Тест нахождения максимума издвух чисел.
+ * @author Shaev Andrey
+ * @version 1.01
+ * @since 18.11.17
+*/
 public class MaxTest {
 @Test
 public void whenFirstLessSecond() {
